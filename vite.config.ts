28033467b5e007a41db0cb94f10fbfdfaf8613ae
Laurent Eschenauer <laurent@eschenauer.be>
2025-04-04 18:14:12 +0200

    Fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => ({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@/components/ui'],
+          ui: ['@/components/ui/button', '@/components/ui/input'],
         },
       },
     },
